8aac76209385c326ebb1d2035ac34f7cfda7f835
TerrencePai <37625652+TerrencePai@users.noreply.github.com>
2022-04-24 15:57:36 +0800

    Add files via upload

--- tiffReadSave.h
+++ tiffReadSave.h
@@ -1,18 +1,21 @@
 #pragma once
-#include <vector>
+#pragma once
 #include <string>
 #include <cstdint>
+#include <cstring>
 #include <fstream>
-#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
-#define MC_GET_16(__data__) ((__data__ & 0xFF) << 8) | (__data__ >> 8)
-#define MC_GET_64(__data__) ((__data__ & 0xFF) << 24) | ((__data__ & 0xFF00) << 8) | ((__data__ & 0xFF0000) >> 8) | ((__data__ & 0xFF000000) >> 24)
+//#define MC_GET_16(__data__) ((__data__ & 0xFF) << 8) | (__data__ >> 8)
+//#define MC_GET_64(__data__) ((__data__ & 0xFF) << 24) | ((__data__ & 0xFF00) << 8) | ((__data__ & 0xFF0000) >> 8) | ((__data__ & 0xFF000000) >> 24)
 
 template<typename T>
 struct complex {
 	T re;
 	T im;
-	complex(): re(T{}), im(T{}) {};
+	complex() : re(T{}), im(T{}) {};
 	complex(T re_) :re(re_), im(T{}) {};
 	complex(T re_, T im_) :re(re_), im(im_) {};
 };
@@ -31,21 +34,46 @@ typedef struct
 	uint32_t valOffset;
 }DE;
 
-typedef struct
+typedef struct IFDclass
 {
 	uint16_t n; // 表示此IFD包含了多少个DE,假设数目为n
 	DE* p; // n个DE
 	uint32_t nextIfdOffset; // 下一个IFD的偏移量,如果没有则置为0
+	IFDclass() :p(nullptr) {};
+	~IFDclass()  {
+		free(p);
+		p = nullptr;
+	};
 }IFD;
+
+typedef enum { whiteIsZeros = 0, BlackIsZeros = 1, RGB = 2, RGBPalette = 3, transparencyMask = 4, KCMYK = 5, YCbCr = 6, CIELAB = 8 }PhotometricInterpretation;
+
 typedef struct imgInfo
 {
-	uint32_t width; // 表示图像宽度
+	PhotometricInterpretation  _PhotometricInterpretation;
+	uint16_t stripsPerImage;
+	uint16_t* BitsPerSample;//
+	uint16_t channel;  // 表示图像通道数
+	uint16_t orientation;
+	uint16_t subFileNum;
 	uint32_t height;  // 表示图像高度
-	uint8_t channel;  // 表示图像通道数
-	uint64_t ImageStart;// 图像数据起始地址存储位置相对于文件开始的位置val的保存位置
-	uint8_t ImageStartTypeSize;//位深
-	uint16_t byteNum;
-	uint16_t typeLabel;
+	uint32_t width; // 表示图像宽度
+	uint32_t* stripOffsets;// 图像条带数据起始地址存储位置相对于文件开始的位置val的保存位置，共有stripsPerImage个
+	uint32_t* stripByteCouts;
+	uint16_t  Compression;//1代表没有压缩、7代表JPEG压缩、32773代表packBits压缩
+	uint32_t rowsPerStrip;
+	uint32_t allBytes;//全部比特数
+	imgInfo() :_PhotometricInterpretation(BlackIsZeros), stripsPerImage(1), channel(1), allBytes(0),
+		orientation(1), subFileNum(1), height(0), width(0), Compression(1), rowsPerStrip(height),
+		BitsPerSample(nullptr),stripOffsets(nullptr),stripByteCouts(nullptr){};
+	~imgInfo() {
+		free(BitsPerSample);
+		BitsPerSample = nullptr;
+		free(stripOffsets);
+		stripOffsets = nullptr;
+		free(stripByteCouts);
+		stripByteCouts = nullptr;
+	}
 }imInfo;
 /*
 * 测试机器是大端(big endian:返回false)还是小端(little endian:返回true)
@@ -56,6 +84,53 @@ inline bool mashineEndian(void)
 	uint16_t* p = &a;
 	return *((uint8_t*)p) == a;
 }
+
+template<typename baseType>
+struct tiffTypeMap {
+	uint16_t typeValue = 1;
+};
+template<>
+struct tiffTypeMap<uint8_t> {
+	uint16_t typeValue = 1;
+};
+template<>
+struct tiffTypeMap<uint16_t> {
+	uint16_t typeValue = 3;
+};
+template<>
+struct tiffTypeMap<uint32_t> {
+	uint16_t typeValue = 4;
+};
+template<>
+struct tiffTypeMap<uint64_t> {
+	uint16_t typeValue = 5;
+};
+template<>
+struct tiffTypeMap<int8_t> {
+	uint16_t typeValue = 6;
+};
+template<>
+struct tiffTypeMap<int16_t> {
+	uint16_t typeValue = 8;
+};
+template<>
+struct tiffTypeMap<int32_t> {
+	uint16_t typeValue = 9;
+};
+template<>
+struct tiffTypeMap<int64_t> {
+	uint16_t typeValue = 10;
+};
+template<>
+struct tiffTypeMap<float> {
+	uint16_t typeValue = 11;
+};
+template<>
+struct tiffTypeMap<double> {
+	uint16_t typeValue = 12;
+};
+
+
 class TiffImg {
 public:
 	imInfo _imInfo;
@@ -63,147 +138,282 @@ public:
 	IFH _IFH;
 	IFD _IFD;
 	TiffImg(string path);
-	void save(string savePath= "defaultSave.tif");
+	template<typename T>
+	TiffImg(T, uint32_t height, uint32_t width, uint8_t channel = 1);
+	void save(string savePath = "defaultSave.tif");
 	~TiffImg() {
 		save();
 		free(data);
 		data = nullptr;
-		free(_IFD.p);
-		_IFD.p = nullptr;
 	}
 	template<typename T>
-	T& operator()(T,uint32_t row, uint32_t col, uint8_t depth=1 ){
-		uint64_t sum = ((row - 1) * _imInfo.width * _imInfo.channel + (col - 1) * _imInfo.channel + (depth - 1)) * _imInfo.ImageStartTypeSize;
+	T& operator()(T, uint32_t row, uint32_t col, uint8_t depth = 1) {
+		uint64_t sum = ((row - 1) * _imInfo.width + (col - 1)) * chanBytes(_imInfo.channel, _imInfo.BitsPerSample) + chanBytes(depth-1, _imInfo.BitsPerSample);
 		return *((T*)(data + sum));
 	}
 private:
-	string Path;
 	bool EndianTrans;
-	bool read();
-
+	bool read(string path, uint32_t IFDoffset=0);
 	const uint16_t littleEndian = 0x4949;
 	const uint16_t bigEndian = 0x4d4d;
+	uint8_t chanBytes(uint16_t channel, uint16_t* BitsPerSample);
 };
+uint8_t TiffImg::chanBytes(uint16_t channel, uint16_t* BitsPerSample) {
+	uint8_t res{};
+	for (int i = 0; i < channel; ++i) {
+		res += (BitsPerSample[i] / 8 + BitsPerSample[i] % 8);
+	}
+	return res;
+};
+template<typename T>
+TiffImg::TiffImg(T, uint32_t height, uint32_t width, uint8_t channel) {
+	_imInfo.height = height;
+	_imInfo.width = width;
+	_imInfo.channel = channel;
+	_imInfo.BitsPerSample = sizeof(T)*8;
+	_imInfo.stripByteCouts[0] = height * width * channel * sizeof(T);
+	if (_imInfo.stripByteCouts[0] % 2 != 0) {
+		++_imInfo.stripByteCouts[0];
+	}
+	uint8_t typeLabel = tiffTypeMap<T>().typeValue;
+	//_imInfo.ImageStart[0] = sizeof(_IFH);
+	_imInfo.stripOffsets[0] = 22550;
+	data = (uint8_t*)malloc(_imInfo.stripByteCouts[0]);
+	EndianTrans = false;
+	_IFH.endian = mashineEndian ? littleEndian : bigEndian;
+	_IFH.magic = 42;
+	_IFH.ifdOffset = _imInfo.stripByteCouts[0] + sizeof(_IFH);
+	_IFH.ifdOffset = sizeof(_IFH);
+	_IFD.n = 6;
+	_IFD.p = (DE*)malloc(_IFD.n * sizeof(DE));
+	memset((void*)_IFD.p, 0, _IFD.n * sizeof(DE));
+	DE* temp = _IFD.p;
+	temp->tag = 0x0100; temp->type; temp->size; temp->valOffset = _imInfo.width;
+	++temp; temp->tag = 0x0101; temp->type; temp->size; temp->valOffset = _imInfo.height;
+	++temp; temp->tag = 0x0102; temp->type = typeLabel; temp->size; temp->valOffset;
+	++temp; temp->tag = 0x0106; temp->type; temp->size; temp->valOffset = _imInfo.channel;
+	++temp; temp->tag = 0x0111; temp->type; temp->size; temp->valOffset = _imInfo.stripOffsets[0];
+	++temp; temp->tag = 0x0117; temp->type; temp->size; temp->valOffset = _imInfo.stripByteCouts[0];
+
+	_IFD.nextIfdOffset = 1;
+}
+
 TiffImg::TiffImg(string path) {
-	Path = path;
-	if (read()) {
-		ifstream frd(Path, ios::in | ios::binary);
+	_IFD.nextIfdOffset = 0;
+	bool  hasNext = true;
+	_imInfo.subFileNum = 0;
+	_imInfo.allBytes = 0;
+	while (read(path,_IFD.nextIfdOffset)&&hasNext) {
+		ifstream frd(path, ios::in | ios::binary);
 		if (frd) {//文件打开成功
-			frd.seekg(static_cast<long>(_imInfo.ImageStart), ios::beg);
-			for (uint32_t row = 0; row < _imInfo.height; ++row) {
-				for (uint32_t col = 0; col < _imInfo.width; ++col) {
-					for (uint8_t dep = 0; dep < _imInfo.channel; ++dep) {
-						uint8_t* temp = data + (row * _imInfo.width * _imInfo.channel + col * _imInfo.channel + dep) * _imInfo.ImageStartTypeSize;
-						frd.read(reinterpret_cast<char*>(temp), _imInfo.ImageStartTypeSize);
-						if (EndianTrans) {//大小端转换,暂未完成
-							;
+			auto chanBytes = [](auto channel, auto  BitsPerSample)->uint8_t {
+				uint8_t res{};
+				for (int i = 0; i < channel; ++i) {
+					res += (BitsPerSample[i] / 8 + BitsPerSample[i] % 8);
+				}
+				return res;
+			};		
+			auto subSum = _imInfo.height * _imInfo.width * chanBytes(_imInfo.channel, _imInfo.BitsPerSample);
+			auto subdat= (uint8_t*)malloc(subSum);
+			memset(subdat, 0, subSum);
+			data = subdat;
+			auto tripBytes = [](auto strips, auto  stripByteCouts)->uint32_t {//i之前的条带字节总数
+				uint32_t res{};
+				for (int i = 0; i < strips; ++i) {
+					res += stripByteCouts[i];
+				}
+				return res;
+			};
+			int tempsum{};
+			for (auto k = 0; k < _imInfo.stripsPerImage; ++k) {
+				frd.seekg(static_cast<long>(_imInfo.stripOffsets[k]), ios::beg);
+				
+				for (auto i = 0; i < _imInfo.stripByteCouts[k];++i) {
+					if (_imInfo.Compression == 1) {
+						frd.read(reinterpret_cast<char*>(subdat+ tempsum), _imInfo.stripByteCouts[k]);
+						tempsum += _imInfo.stripByteCouts[k];
+						break;
+					}else if (_imInfo.Compression == 7) {
+						cerr << "JPEG压缩还没写";
+					}
+					else if (_imInfo.Compression == 32773) {
+						int8_t temp{};
+						frd.read(reinterpret_cast<char*>(&temp), 1);
+						if (temp == -128) {
 						}
+						else if (temp < 0) {
+							uint8_t nextdata{};
+							frd.read(reinterpret_cast<char*>(&nextdata), 1);
+							memset(subdat + tempsum, nextdata, 1-temp);
+							tempsum += 1 - temp;
+							++i;
+						}
+						else {//又可能有问题
+							frd.read(reinterpret_cast<char*>(subdat + tempsum), temp+1);
+							tempsum += 1 + temp;
+							i += temp + 1;
+						}
+
 					}
 				}
+				if (EndianTrans) {//大小端转换,暂未完成
+					cerr << "大小端转换还没写";
+				}
+			
+			}
+			if (!_IFD.nextIfdOffset) {
+				hasNext = false;
 			}
 			frd.close();
+			_imInfo.allBytes +=subSum;
+			++_imInfo.subFileNum;
 		}
 	}
 };
-bool TiffImg::read()
+uint8_t getsize(uint16_t typeLabel){
+	switch (typeLabel) {
+	case 1: ;//Byte
+	case 2:return 1; break;//文本类型，7位Ascii码加1位二进制0
+	case 3:; return sizeof(uint16_t); break;//2字节
+	case 4:  return sizeof(uint32_t); break;//4字节
+	case 5:;  return sizeof(uint64_t); break;//有理数，第一无符号Long为分子，第二个为分母部分
+	case 6:;  return sizeof(int8_t); break;
+	case 8:; return sizeof(int16_t); break;
+	case 9:;  return sizeof(int32_t); break;
+	case 10:;  return sizeof(int64_t); break;///有理数，第一无符号Long为分子，第二个为分母部分
+	case 11:; return sizeof(float); break;
+	case 12:;  return sizeof(double); break;
+	default:;   return 1; break;
+	}
+}
+bool TiffImg::read(string path, uint32_t IfdOffset)
 {
-	ifstream frd(Path, ios::in | ios::binary);
-	if (frd) {//文件打开成功
-		frd.read(reinterpret_cast<char*>(&(_IFH.endian)), sizeof(_IFH.endian));
-		frd.read(reinterpret_cast<char*>(&(_IFH.magic)), sizeof(_IFH.magic));
-		frd.read(reinterpret_cast<char*>(&(_IFH.ifdOffset)), sizeof(_IFH.ifdOffset));
+	ifstream frd(path, ios::in | ios::binary);
+	while(frd) {//文件打开成功
+		frd.seekg(IfdOffset, ios::beg);
+		frd.read(reinterpret_cast<char*>(&(_IFH)), sizeof(_IFH));
 		bool imEndian{ _IFH.endian == littleEndian }; //图像存储方式是大端(false)还是小端(true)
 		EndianTrans = (imEndian != mashineEndian());
 		if (EndianTrans) {
-			_IFH.endian = MC_GET_16(_IFH.endian);
-			_IFH.magic = MC_GET_16(_IFH.magic);
-			_IFH.ifdOffset = MC_GET_64(_IFH.ifdOffset);
+			//_IFH.endian = MC_GET_16(_IFH.endian);
+			//_IFH.magic = MC_GET_16(_IFH.magic);
+			//_IFH.ifdOffset = MC_GET_64(_IFH.ifdOffset);
 		}
 		auto IFD_Start = _IFH.ifdOffset;
 		frd.seekg(static_cast<long>(IFD_Start), ios::beg);
 		frd.read(reinterpret_cast<char*>(&(_IFD.n)), sizeof(_IFD.n));
 		if (EndianTrans) {
-			_IFD.n = MC_GET_16(_IFD.n);
+			;
 		}
 		auto DE_n = _IFD.n;
 		_IFD.p = (DE*)malloc(sizeof(DE) * DE_n);
-		uint32_t DE_val_size{};
-		for (uint32_t i = 0; i < DE_n; ++i) {
-			frd.read(reinterpret_cast<char*>(&(_IFD.p[i].tag)), sizeof(_IFD.p[i].tag));
-			frd.read(reinterpret_cast<char*>(&(_IFD.p[i].type)), sizeof(_IFD.p[i].type));
-			frd.read(reinterpret_cast<char*>(&(_IFD.p[i].size)), sizeof(_IFD.p[i].size));
-			frd.read(reinterpret_cast<char*>(&(_IFD.p[i].valOffset)), sizeof(_IFD.p[i].valOffset));
-			if ((_IFH.endian == littleEndian) != mashineEndian()) {
-				_IFD.p[i].tag = MC_GET_16(_IFD.p[i].tag);
-				_IFD.p[i].type = MC_GET_16(_IFD.p[i].type);
-				_IFD.p[i].size = MC_GET_64(_IFD.p[i].size);
-			}
+		frd.read(reinterpret_cast<char*>(_IFD.p), sizeof(DE)* DE_n);
 
-			if (EndianTrans) {
-				if (DE_val_size == 1) {
-					_IFD.p[i].valOffset = _IFD.p[i].valOffset;
-				}
-				else if (DE_val_size == 2) {
-					_IFD.p[i].valOffset = MC_GET_16(_IFD.p[i].valOffset);
-				}
-				else if (DE_val_size >= 4) {
-					_IFD.p[i].valOffset = MC_GET_64(_IFD.p[i].valOffset);
-				}
+		auto getNdata = [path](auto & DE_ele, auto* res)->void {
+			uint8_t typ=getsize(DE_ele.type);
+			auto DE_val_size= typ * DE_ele.size;
+			if (DE_val_size > 4) {
+				ifstream fr(path, ios::in | ios::binary);
+				fr.seekg(static_cast<long>(DE_ele.valOffset), ios::beg);
+				fr.read(reinterpret_cast<char*>(res), DE_val_size);
+				fr.close();
 			}
-			switch (_IFD.p[i].tag) {
-			case 0x0100: _imInfo.width = _IFD.p[i].valOffset; break;// 图像宽度存储位置
-			case 0x0101: _imInfo.height = _IFD.p[i].valOffset; break;// 图像高度存储位置
-			case 0x0102: {
-				_imInfo.typeLabel = _IFD.p[i].type;
-				switch (_IFD.p[i].type) {
-				case 1:_imInfo.ImageStartTypeSize = 1;//Byte
-				case 2: DE_val_size = (_IFD.p)[i].size; break;//文本类型，7位Ascii码加1位二进制0
-				case 3:   _imInfo.ImageStartTypeSize = 2; DE_val_size = (_IFD.p)[i].size * sizeof(uint16_t); break;//2字节
-				case 4: _imInfo.ImageStartTypeSize = 4; DE_val_size = (_IFD.p)[i].size * sizeof(uint32_t); break;//4字节
-				case 5:   _imInfo.ImageStartTypeSize = 8; DE_val_size = (_IFD.p)[i].size * sizeof(uint64_t); break;//暂不准确：有理数，第一无符号Long为小数，第二个为整数部分
-				case 6:  _imInfo.ImageStartTypeSize = 1; DE_val_size = (_IFD.p)[i].size * sizeof(int8_t); break;
-				case 8:  _imInfo.ImageStartTypeSize = 2; DE_val_size = (_IFD.p)[i].size * sizeof(int16_t); break;
-				case 9:   _imInfo.ImageStartTypeSize = 4; DE_val_size = (_IFD.p)[i].size * sizeof(int32_t); break;
-				case 10:  _imInfo.ImageStartTypeSize = 8; DE_val_size = (_IFD.p)[i].size * sizeof(int64_t); break;///暂不准确：有理数，第一Long为小数，第二个为整数部分
-				case 11: _imInfo.ImageStartTypeSize = 4; DE_val_size = (_IFD.p)[i].size * sizeof(float); break;
-				case 12:  _imInfo.ImageStartTypeSize = 8; DE_val_size = (_IFD.p)[i].size * sizeof(double); break;
-				default: _imInfo.ImageStartTypeSize = 1;  DE_val_size = (_IFD.p)[i].size; break;
-				}
-				break;// 颜色深度  值＝1为单色，＝4为16色，＝8为256色。如果该类型数据个数＞2个，说明是真彩图像
+			else {
+				memcpy(reinterpret_cast<char*>(res), reinterpret_cast<char*>(&DE_ele.valOffset), DE_val_size);
 			}
-			case 0x0103: break;// 图像是否压缩 05表示压缩
-			case 262: {// 表示图像的种类: 0-WhiteisZero 1-BlackisZero 2-RGBImg 3-PaletteColor ...
-				_imInfo.channel = _IFD.p[i].valOffset == 2 ? 3 : 1;
+		};
+		
+		for (uint32_t i = 0; i < DE_n; ++i) {
+			DE & temp = _IFD.p[i];
+			switch (temp.tag) {
+				
+			case 0x00FE: {//新的子文件类型标识 long、1
+				if (temp.valOffset & 1) {
+						//代表是缩略图
+				}
+				if (temp.valOffset & 2) {
+					//代表多页图像中的某一页
+				}
+				if (temp.valOffset & 4) {
+					//代表多页图像中的某一页
+				}
 				break;
 			}
-			case 0x0111: {// 图像数据起始地址存储位置相对于文件开始的位置val的保存位置
-				_imInfo.ImageStart = _IFD.p[i].valOffset;
+			case 0x00FF: {//子文件类型标识 short、1  （过时tag）
+				if (temp.valOffset ==1) {
+					//代表是全分辨率图像
+				}
+				if (temp.valOffset== 2) {
+					//代表缩小分辨率图像
+				}
+				if (temp.valOffset==3) {
+					//代表多页图像中的某一页
+				}
+				break;
+			}	  
+			case 0x0100: getNdata(temp,&_imInfo.width); break;// 图像宽度存储位置
+			case 0x0101: getNdata(temp, &_imInfo.height); break;// 图像高度存储位置
+			case 0x0102: {//每个通道的Bit数 SHORT ，长度为SamplesPerPixel
+				_imInfo.channel = temp.size;
+				_imInfo.BitsPerSample = new uint16_t[_imInfo.channel];
+				getNdata(temp, _imInfo.BitsPerSample);
 				break;
 			}
-			case 274: break;// 图像坐标系方式: 1[left top] 2[right top] 3[bottom right] ...
-			case 277: break;
-			case 278: break;// 表示每一个Strip内包含的图像的行数 eg:Img[w][h][w] --> RowsPerStrip=1
+			case 0x0103: {// 图像是否压缩 05表示压缩 short、1
+				getNdata(temp, &_imInfo.Compression);
+				break;
+			}
+			case 0x0106: {// 表示图像的种类
+				_imInfo._PhotometricInterpretation=(PhotometricInterpretation)_IFD.p[i].valOffset;
+				break;
+			}
+			case 0x0111: {// 图像条带数据起始地址存储位置相对于文件开始的位置val的保存位置
+				_imInfo.stripsPerImage = _IFD.p[i].size;
+				_imInfo.stripOffsets = new uint32_t[_imInfo.stripsPerImage];
+				getNdata(temp, _imInfo.stripOffsets);
+				break;
+			}
+			case 0x0112: {// 
+				getNdata(temp, &_imInfo.orientation);
+				break;
+			}
+			case 0x0114: break;// 图像坐标系方式: 1[left top] 2[right top] 3[bottom right] ...
+			case 0x0115:getNdata(_IFD.p[i], &_imInfo.channel);  break;//像素通道数 short 、1
+			case 0x0116: {
+				getNdata(temp, &_imInfo.rowsPerStrip);
+				break;// 表示每一个Strip内包含的图像的行数 eg:Img[w][h][w] --> RowsPerStrip=1
+			}
 			case 0x0117: {
-				_imInfo.byteNum = _IFD.p[i].valOffset;
+				_imInfo.stripsPerImage = _IFD.p[i].size;
+				_imInfo.stripByteCouts = new uint32_t[_imInfo.stripsPerImage];
+				getNdata(temp, _imInfo.stripByteCouts);
 				break; // 图像字节总数，非偶数后面补零
 			}
 			case 284: break;// 全彩图像每一个像素点排列方式: 1[RGB...RGB]-交叉 2[[R...R],[G...G],[B...B]]-平铺
+			case 0x0128:
+				break;
+			case 0x011A:
+				//X分辨率，5：rational
+				break;
+			case 0x011B:
+				break;//Y分辨率，5：rational
 			default:  break;
 			}
 		}
+		frd.read(reinterpret_cast<char*>(&_IFD.nextIfdOffset), sizeof(_IFD.nextIfdOffset));
 		frd.close();
-		data =(uint8_t *)malloc(_imInfo.height* _imInfo.width* _imInfo.channel* _imInfo.ImageStartTypeSize);
 		return true;
 	}
 	return false;
 }
 void TiffImg::save(string savePath) {
+
 	ofstream fd{ savePath, ios::out | ios::binary };
 	if (fd.is_open()) {
-		fd.seekp(0L,ios::beg);
+		fd.seekp(0L, ios::beg);
 		if (EndianTrans) {
-			uint16_t Endian =(_IFH.endian==littleEndian ? bigEndian: littleEndian);
-			fd.write((char*)(& (Endian)), sizeof(_IFH.endian));
+			uint16_t Endian = (_IFH.endian == littleEndian ? bigEndian : littleEndian);
+			fd.write((char*)(&(Endian)), sizeof(_IFH.endian));
 		}
 		else {
 			fd.write((char*)&(_IFH.endian), sizeof(_IFH.endian));
@@ -212,21 +422,11 @@ void TiffImg::save(string savePath) {
 		fd.write((char*)&(_IFH.ifdOffset), sizeof(_IFH.ifdOffset));
 		fd.seekp(_IFH.ifdOffset, ios::beg);
 		fd.write((char*)&(_IFD.n), sizeof(_IFD.n));
-		for (uint16_t i = 0; i < _IFD.n; ++i) {//仅仅适用于有一个IFH的文件
-			fd.write((char*)( & (_IFD.p[i].tag)),sizeof(_IFD.p->tag));
-			fd.write((char*)(&(_IFD.p[i].type)), sizeof(_IFD.p->type));
-			fd.write((char*)(&(_IFD.p[i].size)), sizeof(_IFD.p->size));
-			fd.write((char*)(&(_IFD.p[i].valOffset)), sizeof(_IFD.p->valOffset));
-		}
-		fd.write((char*)(&(_IFD.nextIfdOffset)),sizeof(_IFD.nextIfdOffset)); // 下一个图像文件的入口位置，一般对于单一文件，这里为0
-		fd.seekp(_imInfo.ImageStart,ios::beg);
-		uint64_t byteNum = _imInfo.height * _imInfo.width * _imInfo.channel * _imInfo.ImageStartTypeSize;
-		for (uint64_t i = 0; i < byteNum;++i) {//写图像数据
-			fd.write((char*)(&(data[i])), sizeof(data[i]));
-		}
+		fd.write((char*)(_IFD.p), sizeof(DE)* _IFD.n);
+		fd.write((char*)(&(_IFD.nextIfdOffset)), sizeof(_IFD.nextIfdOffset)); // 下一个图像文件的入口位置，一般对于单一文件，这里为0
+		fd.seekp(_imInfo.stripOffsets[0], ios::beg);
+		fd.write((char*)(data), _imInfo.allBytes);
 		fd.close();
 	}
 
 }
-
-
